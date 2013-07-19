133f32730ceef51394157c6d350c4be9fe11eee5
Oleksiy Ivanov <oleksiy.ivanov@bromium.com>
2013-07-19 20:53:17 +0100

    Initial Commit

---
+++ NiceJourney/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NiceJourney
+//
+//  Created by Oleksiy Ivanov on 19/07/2013.
+//  Copyright (c) 2013 Oleksiy Ivanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
